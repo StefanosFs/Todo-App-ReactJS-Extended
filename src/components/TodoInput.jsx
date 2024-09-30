0b6c143eee6e68d2176d091afcc643330816260b
CyberEnigma7 <Stef07codes@gmail.com>
2024-09-30 10:02:41 -0500

    first commit

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+
+const TodoInput = (props) => {
+  const {handleAddTodos, todoValue, setTodoValue} = props
+  
+  return (
+    <header>
+      <input placeholder="Enter todo..." value={todoValue} onChange={(e) => {
+        setTodoValue(e.target.value)
+      }} />
+      <button onClick={() => {
+        handleAddTodos(todoValue)
+        setTodoValue('')
+      }}>Add</button>
+    </header>
+  )
+}
+
+export default TodoInput
